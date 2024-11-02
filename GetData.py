5eeb0ec0a68828f1e40393b37c840d542c71050a
harry-cheema07 <cheema.harkiratsingh@gmail.com>
2024-11-02 19:12:32 -0400

    Price and volume of ticker

--- GetData.py
+++ GetData.py
@@ -5,5 +5,6 @@ import yfinance as yf
 def getData(ticker):
     tickInfo = yf.Ticker(ticker)
     hist = tickInfo.history(period="1mo")
+    print(hist)
     return hist
 
--- VisualizeData.py
+++ VisualizeData.py
@@ -1,15 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import GetData as gd
+ticker='AAPL'
+data=gd.getData(ticker)
 
-data=gd.getData('MSFT')
+y1=data['Close']
+y2=data['Volume']
+x1=data['Close'].index
+BarColor = []
 
-plt.figure(figsize=(10, 6))  # Set the size of the figure
-plt.plot((data['Close'].index), (data['Close']), marker='o')  # Line plot
-plt.title('Line Graph Title')  # Title of the graph
-plt.xlabel('Date')  # X-axis label
-plt.ylabel('Closed Price')  # Y-axis label
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-plt.grid(True)  # Add grid lines for better visibility
-plt.tight_layout()  # Adjust layout to avoid clipping
-plt.show()  # Display the plot
+Price=0
+for y in y1:
+    if y >= Price:
+        BarColor.append('green')
+        Price = y
+    else:
+        BarColor.append('red')
+        Price = y
+print(BarColor)
+
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+
+# Plotting the line chart
+ax1.plot(x1, y1, color='blue', marker='o', label='Sine Wave')
+ax1.set_title('Price Chart of '+ticker)
+ax1.set_ylabel('Close Price', color='blue')
+ax1.tick_params(axis='y', labelcolor='blue')
+
+
+# Create a second y-axis for the bar chart
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+bars=ax2.bar(x1, y2, color=BarColor, alpha=0.6, label='Random Values')
+for bar in bars:
+    yval = bar.get_height()  # Get the height of each bar
+    ax2.text(bar.get_x(), yval/2, str(yval), ha='center', va='center',rotation=90)  # Annotate the bar
+
+
+
+ax2.set_ylabel('Volume', color='orange')
+ax2.tick_params(axis='y', labelcolor='orange')
+
+# Show plot
+plt.show()
+
+

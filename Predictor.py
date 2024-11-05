7d6a1d130fa9af592724b95de4eb8f36fc3dfed3
harry-cheema07 <cheema.harkiratsingh@gmail.com>
2024-11-05 08:55:58 -0500

    the MSE is too big

--- Predictor.py
+++ Predictor.py
@@ -19,29 +19,51 @@ Wrsi=2
 Wv=3
 b=4
 
-PredictedPrice=SMA.values*Wsma+RSI.values*Wrsi+V.values.flatten()*Wv+b
+
 
 ActualPrice=gd.getClosePrice(ticker,startDate,endDate)
 
 #Setting first few values as null because Moving averages will be null for first few records
 ActualPrice[:SMAValue-1]=np.nan
-
-
-#Cleaning the Arrays from null values
-PredictedPrice = PredictedPrice[~np.isnan(PredictedPrice)]
 ActualPrice = ActualPrice[~np.isnan(ActualPrice).any(axis=1)]
 
+#Cleaning the Arrays from null values
 
-MeanSquaredError = (ActualPrice.values.flatten() - PredictedPrice)*(ActualPrice.values.flatten() - PredictedPrice)
+
+
 #print(PredictedPrice)
 #print(ActualPrice.values.flatten())
 #print(MeanSquaredError)
 
-##Trying to calculate deravative here
-sum = 0
-for pp,ap,sm in zip(PredictedPrice,ActualPrice.values.flatten(),SMA.values[SMAValue-1:]):
-     sum = sum + (ap-pp) * sm
+for i in range(10):
+     PredictedPrice=SMA.values*Wsma+RSI.values*Wrsi+V.values.flatten()*Wv+b
+     PredictedPrice = PredictedPrice[~np.isnan(PredictedPrice)]
+     ##Trying to calculate deravative here
+     sum = 0
+     for pp,ap,sm in zip(PredictedPrice,ActualPrice.values.flatten(),SMA.values[SMAValue-1:]):
+          sum = sum + (ap-pp) * sum
+     drvWsma = (-2/len(PredictedPrice))*sum
 
-drvWsma = (-2/len(PredictedPrice))*sum
+     sum = 0
+     for pp,ap,vol in zip(PredictedPrice,ActualPrice.values.flatten(),V.values.flatten()[SMAValue-1:]):
+          sum = sum + (ap-pp) * vol
+     drvWv = (-2/len(PredictedPrice))*sum
 
-print(drvWsma)
+     sum = 0
+     for pp,ap,rs in zip(PredictedPrice,ActualPrice.values.flatten(),RSI.values[SMAValue-1:]):
+          sum = sum + (ap-pp) * rs
+
+     drvWrsi = (-2/len(PredictedPrice))*sum
+
+     #Updating the weights
+
+     Wsma=Wsma - learningRate * drvWsma
+     Wrsi=Wrsi - learningRate * drvWrsi
+     Wv=Wv - learningRate * drvWv
+
+
+
+
+MeanSquaredError = (ActualPrice.values.flatten() - PredictedPrice)*(ActualPrice.values.flatten() - PredictedPrice)
+
+print(MeanSquaredError)

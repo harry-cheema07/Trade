34a923bd47458de94d532f02cced375fb3ea070a
harry-cheema07 <cheema.harkiratsingh@gmail.com>
2024-11-11 15:55:24 -0500

    Checking predictions using close price only

--- Insights.py
+++ Insights.py
@@ -5,6 +5,7 @@ import numpy as np
 import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_pacf,plot_acf
 from statsmodels.tsa.arima.model import ARIMA
+from pmdarima import auto_arima
 
 SMAValue=50
 startDate='2023-01-01'
@@ -73,27 +74,65 @@ fig, ax =plt.subplots(figsize=(10, 6))
 first_diffs = ActualPrice.values[1:] - ActualPrice.values[:-1]
 first_diffs = np.concatenate([first_diffs.flatten(), [0]])
 ActualPrice['diff'] = first_diffs
-print(len(ActualPrice.index))
-print(len(ActualPrice['diff']))
+
 #Plotting the stationary data
 #plt.plot(ActualPrice.index,ActualPrice['diff'],color='blue')
-print(ActualPrice['diff'])
+
 #Calculating PACF value
-pacf_values=sm.tsa.adfuller(ActualPrice['diff'])
-print(pacf_values)
+#pacf_values=sm.tsa.adfuller(ActualPrice['diff'])
+
 #PACF PLOT
 #plot_pacf(ActualPrice['diff'],ax=ax,lags=100)
 
 
 #ACF PLOT
-plot_acf(ActualPrice['diff'],ax=ax,lags=100)
+#plot_acf(ActualPrice['diff'],ax=ax,lags=100)
+#plt.show()
+
+
+#Calculate the right order
+#stepwise_fit=auto_arima(ActualPrice['diff'],trace=True,suppress_warnings=True)
+
+#stepwise_fit.summary()
+
+# Best model:  ARIMA(2,0,2)(0,0,0)[0] intercept
+
+#Create Training and Testing data set
+
+training = ActualPrice['diff'].iloc[:-30]
+testing = ActualPrice['diff'].iloc[-30:]
+
+print(training)
+
+print(testing)
+
+
+
+p,d,q=2,0,2
+# Fit an ARIMA model
+model = ARIMA(training, order=(p,d,q))  # Replace (p,d,q) with appropriate values
+model_fit = model.fit()
+
+# Summary of the model
+#print(model_fit.summary())
+
+
+#Predicting values of testing set
+
+start = len(training)
+end = len(training) + len(testing) - 1
+pred = model_fit.predict(start=start, end=end, type = 'levels')
+
+
+#Reversing Difference
+predicted_values = pred.cumsum() + ActualPrice[ticker][1:].iloc[:-30][-1]
+
+Result = pd.DataFrame(data = ActualPrice[1:][-30:][ticker].values,index = DateValue.values[1:][-30:], columns=['Actual'])
+Result['Predicted']=predicted_values.values
+print(Result)
+
+#The Result shows the price cannot be predicted only using Close Price as the feature
+
+plt.plot(Result,label=['Actual','Predicted'])
+plt.legend()
 plt.show()
-
-
-# p,d,q=0,0,0
-# # Fit an ARIMA model
-# model = ARIMA(ActualPrice['diff'], order=(p,d,q))  # Replace (p,d,q) with appropriate values
-# model_fit = model.fit()
-
-# # Summary of the model
-# print(model_fit.summary())
